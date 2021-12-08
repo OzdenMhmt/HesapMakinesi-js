b4ba8f71159f2134bc2df52f3a3885372828a005
Mehmet <m.sozdens@gmail.com>
2021-12-09 02:03:50 +0300

    v1

---
+++ custom.js
@@ -0,0 +1,18 @@
+var optDurum=false, opt="", sonuc=0;
+
+
+//Nesneleri olusturma;
+
+var button =document.querySelectorAll(".btn");
+var txtGosterge=document.querySelector("#gosterge");
+
+console.log(button);
+
+button.forEach(function (element) {
+    element.onclick =function (e) {
+        if (txtGosterge.textContent) {
+            
+        }
+        
+    }
+});
